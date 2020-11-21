a4dc59988362cabd59bd0c3e90ce18e6b5bddd35
rainbowCat <jazziblue@gmail.com>
2020-11-21 00:48:41 -0800

    fix shifting in alignment

--- rectification.py
+++ rectification.py
@@ -5,7 +5,7 @@ import filters
 
 def blend(im1, im2, method):
     """ Blend two images together """
-    if method is "two-band":
+    if method == "two-band":
         return two_band_blend(im1, im2)
     else:
         return None
@@ -19,31 +19,40 @@ def two_band_blend(im1, im2):
     high2 = filters.unsharp_mask_filter(im2)
     return np.sum([low1, low2, high1, high2])
 
+def align(warped_im1, warped_im2, warped_im1_pts, warped_im2_pts):
+    """ 
+    Returns two separate images with padding added for alignment.
+    Input points should be standardized. (using the same coordinate system / warped)
+    """
+    im1, im2 = warped_im1, warped_im2
+    im1_pts = np.int32(np.round(warped_im1_pts))
+    im2_pts = np.int32(np.round(warped_im2_pts))
+    aligned_pts1, aligned_pts2 = im1_pts.copy(), im2_pts.copy()
 
-def align(im1, im2, im1_pts, im2_pts):
-    """ Return two separate images with padding added for alignment. """
-    im1_pts = np.int32(np.round(im1_pts))
-    im2_pts = np.int32(np.round(im2_pts))
-    
     # w, h = x, y
     p1_w, p1_h = im1_pts[:, 0].max(), im1_pts[:, 1].max()
     p2_w, p2_h = im2_pts[:, 0].max(), im2_pts[:, 1].max()
-    
+
     im1_pad, im2_pad = [[0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0]]
+
     h_diff, w_diff = abs(p1_h - p2_h), abs(p1_w - p2_w)
     if p1_h < p2_h:
         im1_pad[0][0] = h_diff  # pad before
-        im2_pad[0][1] = h_diff  # pad after
+#         im2_pad[0][1] = h_diff  # pad after
+        algined_pts1[:, 1] += h_diff
     else:
         im2_pad[0][0] = h_diff  # pad before
-        im1_pad[0][1] = h_diff  # pad after
+#         im1_pad[0][1] = h_diff  # pad after
+        aligned_pts2[:, 1] += h_diff
 
     if p1_w < p2_w:
         im1_pad[1][0] = w_diff  # pad before
-        im2_pad[1][1] = w_diff  # pad after
+#         im2_pad[1][1] = w_diff  # pad after
+        algined_pts1[:, 0] += w_diff
     else:
         im2_pad[1][0] = w_diff  # pad before
-        im1_pad[1][1] = w_diff  # pad after
+#         im1_pad[1][1] = w_diff  # pad after
+        aligned_pts2[:, 0] += w_diff
     
     im1_pad = tuple((before, after) for before, after in im1_pad)
     im2_pad = tuple((before, after) for before, after in im2_pad)
