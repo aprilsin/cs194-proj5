7ef0add3494858e1a787ceea91260e559b71e3fc
rainbowCat <jazziblue@gmail.com>
2020-11-20 23:35:55 -0800

    write align and blend functions

--- rectification.py
+++ rectification.py
@@ -1,7 +1,58 @@
-def blend(im1, im2, alpha):
+import numpy as np
+import filters
+
+
+def blend(im1, im2, method):
     """ Blend two images together """
-    pass
+    if method is "two-band":
+        return two_band_blend(im1, im2)
+    else:
+        return None
 
 
-def rectify(im1, im2):
-    pass
+def two_band_blend(im1, im2):
+    assert im1.shape == im2.shape
+    low1 = filters.gaussian_filter(im1)
+    low2 = filters.gaussian_filter(im2)
+    high1 = filters.unsharp_mask_filter(im1)
+    high2 = filters.unsharp_mask_filter(im2)
+    return np.sum([low1, low2, high1, high2])
+
+
+def align(im1, im2, im1_pts, im2_pts):
+    """ Return two separate images with padding added for alignment. """
+    h1, w1, c1 = im1.shape
+    h2, w2, c2 = im2.shape
+    p1_h, p1_w = im1_pts[:, 0].max(), im1_pts[:, 1].max()
+    p2_h, p2_w = im2_pts[:, 0].max(), im2_pts[:, 1].max()
+
+    im1_pad, im2_pad = ((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0))
+    h_diff, w_diff = abs(p1_h - p2_h), abs(p1_w - p2_w)
+    if p1_h < p2_h:
+        im1_pad[0][0] = h_diff  # pad before
+        im2_pad[0][1] = h_diff  # pad after
+    else:
+        im2_pad[0][0] = h_diff  # pad before
+        im1_pad[0][1] = h_diff  # pad after
+
+    if p1_w < p2_w:
+        im1_pad[1][0] = w_diff  # pad before
+        im2_pad[1][1] = w_diff  # pad after
+    else:
+        im2_pad[1][0] = w_diff  # pad before
+        im1_pad[1][1] = w_diff  # pad after
+
+    im1_aligned = np.pad(im1, im1_pad)
+    im2_aligned = np.pad(im2, im2_pad)
+
+    assert im1_aligned.shape == im2_aligned.shape
+    return im1_aligned, im2_aligned
+
+def blend_windows(aligned1_pts, aligned2_pts):
+    x, y = aligned1_pts[:, 0], aligned2_pts[:, 1]
+
+def stitch(im1, im2, im1_pts, im2_pts, blend_method="two-band"):
+    """ Stictch two warped images. All inputs should be warped. """
+    im1, im2 = align(im1, im2, im1_pts, im2_pts)
+    mosaic = blend(im1, im2, blend_method)
+    return mosaic
