378ca89951e0980eaf60782509515dd1595ff953
rainbowCat <jazziblue@gmail.com>
2020-11-26 19:48:48 -0800

    debug descriptor

--- descriptor.py
+++ descriptor.py
@@ -16,10 +16,12 @@ def refine_matches():
 def get_patches(img, corners) -> np.ndarray:
     """make all detected corner an 8x8 grid"""
     img = filters.gauss_blur(img)
-    img = utils.to_gray(img)  # TODO make it work with colored patches
+    img = utils.to_gray(img)  # TODO make it work with colored patches?
 
     patches = []
-    for (r, c) in corners:
+    print(corners[4])
+    for (y, x) in corners:
+        r, c = y, x
         patch = img[r - 20 : r + 20, c - 20 : c + 20]  # get a 40x40 patch
         patch = patch[::5, ::5]  # downsample by taking every 5th pixel
         patch = utils.normalize(patch)  # normalize
