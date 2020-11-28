a7dd08cde152504e3fab73a300d9bfc8a63fd967
Alok Singh <alokbeniwal@gmail.com>
2020-11-27 17:15:31 -0800

    update ransac and feature matching

--- matching.py
+++ matching.py
@@ -1,8 +1,10 @@
 import itertools
+import random
 import sys  # TODO remove this
 
 import numpy as np
 import skimage.transform
+from scipy.spatial.distance import cdist
 
 import constants
 import filters
@@ -16,44 +18,20 @@ def match_features(coords1, patches1, coords2, patches2, threshold=MATCHING_THRE
     assert len(coords1) == len(patches1), f"{len(coords1)}, {len(patches1)}"
     assert len(coords2) == len(patches2), f"{len(coords2)}, {len(patches2)}"
 
-    matched_indices = []
-    ssd = utils.dist2(patches1, patches2)
-    is_candidate = np.full(shape=(len(coords1), len(coords2)), fill_value=True)
+    ssd = cdist(patches1, patches2, metric="sqeuclidean")
+    matched_indices = set()
 
-    # while is_candidate.any():
-    # break
+    for i in range(len(coords1)):
+        first, second, *_ = np.argsort(ssd[i]).ravel()
 
-    for i in range(len(coords1)):  # for each corner in image 1
-        best_match_ind = None
-        best_match_dist = float("inf")
-        second_match_ind = None
-        second_match_dist = float("inf")
+        if ssd[i, first] / ssd[i, second] < threshold:
+            matched_indices.add((i, first))
 
-        for j in range(len(coords2)):  # for each corner in image 2
-            if is_candidate[i, j]:  # TODO how to break out of two loops?
-                dist = ssd[i, j]
-
-                if dist < best_match_dist:
-                    second_match_ind = best_match_ind
-                    second_match_dist = best_match_dist
-                    best_match_ind = j
-                    best_match_dist = dist
-
-                elif dist < second_match_dist:
-                    second_match_dist = dist
-                    second_match_ind = j
-
-                if best_match_dist / second_match_dist < threshold:
-                    matched_indices.append([i, j])
-                    is_candidate[i, :] = False
-                    is_candidate[:, j] = False
-
-    matched1 = np.array([coords1[i] for i, j in matched_indices])
-    matched2 = np.array([coords2[j] for i, j in matched_indices])
+    matched1 = np.array([coords1[i] for i, _ in matched_indices])
+    matched2 = np.array([coords2[j] for _, j in matched_indices])
 
     if constants.DEBUG:
         print(matched1[0], type(matched1[0]))
-
     assert len(matched1) == len(matched2)
     utils.assert_coords(matched1)
     utils.assert_coords(matched2)
@@ -65,7 +43,6 @@ def ransac(corners1, corners2, epsilon=RANSAC_THRESHOLD):
         len(corners1),
         len(corners2),
     )
-    num_input_matches = len(corners1)
 
     corners1 = np.array(corners1)
     corners2 = np.array(corners2)
@@ -75,12 +52,12 @@ def ransac(corners1, corners2, epsilon=RANSAC_THRESHOLD):
     best_inliers1, best_inliers2 = [], []
 
     # select NUM_SAMPLE_POINTS points at random to compute homography
-    for indices in itertools.combinations(
-        range(num_input_matches), constants.NUM_SAMPLE_POINTS
-    ):
+    for _ in range(1_000):
+        chosen1, chosen2 = [
+            c[np.random.choice(len(c), replace=False, size=constants.NUM_SAMPLE_POINTS)]
+            for c in (corners1, corners2)
+        ]
         # compute homography
-        chosen1 = [corners1[i] for i in indices]
-        chosen2 = [corners2[i] for i in indices]
         h_matrix = homography.homo_matrix(chosen1, chosen2)
 
         # compute inliers and count number of coordinates that are good matches
@@ -88,15 +65,17 @@ def ransac(corners1, corners2, epsilon=RANSAC_THRESHOLD):
         dist = utils.ssd_points(
             corners2, predicted2
         )  # compare predicted with ground truth
+
         if constants.DEBUG:
             print(min(dist), max(dist))
+
         matches = dist < epsilon
 
-        num_matches = np.sum(matches)
+        num_matches = matches.sum()
         if constants.DEBUG:
             print(num_matches)
 
-        # save inliners if they are the largest set so far
+        # save inliers if they are the largest set so far
         if num_matches > best_num_inliers:
             best_inliers1 = corners1[matches]
             best_inliers2 = corners2[matches]
